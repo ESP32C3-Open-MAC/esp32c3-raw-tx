caa4ba1e5c9665efb73803571752121e7383382b
s8ismudr <s8ismudr@stud.uni-saarland.de>
2024-08-23 13:50:55 +0200

    Killing propietary wifi thread

--- main/wifi_conn_main.c
+++ main/wifi_conn_main.c
@@ -20,17 +20,61 @@ library. Depends on only FreeRTOS and the standard ESP32 libraries
 #include "esp_err.h"
 #include "esp_log.h"
 
+#define QEMU_AP
+// #undef QEMU_AP
+
 // Defining connection parameters
+#ifdef QEMU_AP
 #define WIFI_SSID "Espressif"
 #define WIFI_PASS ""
+#else
+#define WIFI_SSID "ResPECT"
+#define WIFI_PASS "twoflower"
+#endif
+
 #define SCAN_AUTH_MODE_THRESHOLD ESP_WIFI_AUTH_WPA2_PSK // Not used since it is default
 #define MAX_RETRY 5
+
+typedef uint8_t macaddr_t[6];
+
+// Struct for 80211 packet format
+typedef struct mac80211_frame {
+    struct mac80211_frame_control {
+        unsigned    protocol_version    : 2;
+        unsigned    type            : 2;
+        unsigned    sub_type        : 4;
+        unsigned    to_ds           : 1;
+        unsigned    from_ds         : 1;
+        unsigned    _flags:6;
+    } __attribute__((packed)) frame_control;
+    uint16_t  duration_id;
+    macaddr_t receiver_address;
+    macaddr_t transmitter_address;
+    macaddr_t address_3;
+    struct mac80211_sequence_control {
+        unsigned    fragment_number     : 4;
+        unsigned    sequence_number     : 12;
+    } __attribute__((packed));
+    uint8_t data_and_fcs[2316];
+}  __attribute__((packed)) mac80211_frame;
+
+typedef struct dma_list_item {
+	uint16_t size : 12;
+	uint16_t length : 12;
+	uint8_t _unknown : 6;
+	uint8_t has_data : 1;
+	uint8_t owner : 1; // What does this mean?
+	void* packet;
+	struct dma_list_item* next;
+} __attribute__((packed)) dma_list_item;
+
+extern bool pp_post(uint32_t requestnum, uint32_t argument);
 
 esp_err_t ret;
 
 uint8_t s_retry_num = 0;
 
-
+// Hand crafted UDP packet
 const uint8_t packet[] = {
     // MAC layer
     0x74, 0xdf, 0xbf, 0xa4, 0xf7, 0x97, // Destination MAC address
@@ -57,6 +101,8 @@ const uint8_t packet[] = {
     'h', 'e', 'l', 'l', 'o' // The message :)
 
 };
+
+
 
 // Event handlers for connecting to the wifi. If event notifies that station is started, perform connection
 // esp_wifi_connect() is part of the blobs
@@ -116,16 +162,8 @@ void app_main(){
     ESP_ERROR_CHECK(esp_wifi_start());
 
     vTaskDelay(2000 / portTICK_PERIOD_MS);
-    
-    // while(1){
-        // Prepare a packet buffer for sending
-    uint8_t psize = sizeof(packet);
-    
-    // Call the internal tx function (also from blob)
-    ret = esp_wifi_internal_tx(WIFI_IF_STA, &packet, psize);
-    ESP_LOGI("main", "Sent with result %s", esp_err_to_name(ret));
 
-    vTaskDelay(2000 / portTICK_PERIOD_MS);
-    // }
-    
+    ESP_LOGW("main", "Killing proprietary wifi task (ppTask)");
+	pp_post(0xf, 0);
+
 }

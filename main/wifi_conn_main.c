f28ecb30c66cf0082980e1a03349272d33c9bf69
s8ismudr <s8ismudr@stud.uni-saarland.de>
2024-08-28 12:21:15 +0200

    Added sdkconfig.defaults, kconfig and updated gitignore

--- main/wifi_conn_main.c
+++ main/wifi_conn_main.c
@@ -1,6 +1,7 @@
 /*ResPECT - Transactional networking stack development.
-ESP32 - Firmware for transmitting a UDP packet bypassing the esp_netif()
-library. Depends on only FreeRTOS and the standard ESP32 libraries 
+ESP32 - POC sketch to transmit a packet using register access. 
+Depends on the esp_wifi library for initialization.
+FreeRTOS is still needed
 */
 
 // Standard libraries
@@ -11,7 +12,7 @@ library. Depends on only FreeRTOS and the standard ESP32 libraries
 
 // WiFi driver library
 #include "esp_private/wifi.h"
-#include "esp_wifi.h"
+#include "esp_wifi.h" // Needed for init tasks
 
 // Event library
 #include "esp_event.h"
@@ -20,13 +21,8 @@ library. Depends on only FreeRTOS and the standard ESP32 libraries
 #include "esp_err.h"
 #include "esp_log.h"
 
-// Defining connection parameters
-// #define WIFI_SSID "Espressif"
-// #define WIFI_PASS ""
-#define WIFI_SSID "ResPECT"
-#define WIFI_PASS "twoflower"
+#include "sdkconfig.h"
 
-#define SCAN_AUTH_MODE_THRESHOLD ESP_WIFI_AUTH_WPA2_PSK // Not used since it is default
 #define MAX_RETRY 5
 
 // Register definitions
@@ -37,36 +33,7 @@ library. Depends on only FreeRTOS and the standard ESP32 libraries
 #define WIFI_TX_PLCP1_2 0x600342fc
 #define WIFI_TX_PLCP2 0x60034314
 #define WIFI_TX_DURATION 0x60034318
-// #define WIFI_DMA_INT_STATUS 0x60033c3c
-// #define WIIF_DMA_INT_CLR 0x60033c40
-#define WIFI_TXQ_GET_STATE 0x60033cb0
 
-#define SEND_RAW
-
-// typedef uint8_t macaddr_t[6];
-
-// // Struct for 80211 packet format
-// typedef struct mac80211_frame {
-//     struct mac80211_frame_control {
-//         unsigned    protocol_version    : 2;
-//         unsigned    type            : 2;
-//         unsigned    sub_type        : 4;
-//         unsigned    to_ds           : 1;
-//         unsigned    from_ds         : 1;
-//         unsigned    _flags:6;
-//     } __attribute__((packed)) frame_control;
-//     uint16_t  duration_id;
-//     macaddr_t receiver_address;
-//     macaddr_t transmitter_address;
-//     macaddr_t address_3;
-//     struct mac80211_sequence_control {
-//         unsigned    fragment_number     : 4;
-//         unsigned    sequence_number     : 12;
-//     } __attribute__((packed));
-//     uint8_t data_and_fcs[2316];
-// }  __attribute__((packed)) mac80211_frame;
-
-#ifdef SEND_RAW
 typedef struct dma_list_item {
 	uint16_t size : 12;
 	uint16_t length : 12;
@@ -77,52 +44,24 @@ typedef struct dma_list_item {
 	struct dma_list_item* next;
 } __attribute__((packed,aligned(4))) dma_list_item_t;
 
-extern bool pp_post(uint32_t requestnum, uint32_t argument);
-#endif
-
 esp_err_t ret;
 
 uint8_t s_retry_num = 0;
 
-// // // Hand crafted UDP packet
-#ifndef SEND_RAW
-const uint8_t udp_packet[] = {
-    // MAC layer
-    0xc8, 0x15, 0x4e, 0xd4, 0x65, 0x1b,
-    0x84, 0xf7, 0x03, 0x60, 0x81, 0x5c, // Source MAC address
-    0x08, 0x00, // protocol type - IPV4
-    
-    // IPv4 header
-    0x45, //version :4 (obv) with IHL = 5
-    0x00, // DSCP and ECN
-    0x00, 0x21, // Total length (33 bytes), IPv4 Header + UDP Header + "Hello"
-    0x00, 0x00, // Identification and fragmentation data
-    0x00, 0x00, // Flags and fragment offset
-    0x05, // TTL
-    0x11, // Protocol type UDP 
-    0x33, 0x55, // Header checksum (update after calculating length of total packet)
-    0xc0, 0xa8, 0x00, 0x7A, // Source IP address - Arbitrary IP address for the ESP-32
-    0xc0, 0xa8, 0x00, 0xb7, // Destination IP address - Laptop's IP address (assigned through DHCP by the router)
-
-    // UDP header - 8 bytes
-    0x1f, 0x45, // Source port - Both source and destination ports are random since it is UDP
-    0xf0, 0xf0, // Destination port
-    0x00, 0x0d, // Length
-    0x00, 0x00,  // Checksum - calculate using the pseudo ipv4 header
-    'h', 'e', 'l', 'l', 'o' // The message :)
-
-};
-#else
-// esp32-open-mac frame format
+// Hand crafted 80211 packet. Currently only visible in airmon-ng
 uint8_t packet[] = {
     0x08, 0x01, // frame control
     0x00, 0x00, // duration/ID
-    0xc8, 0x15, 0x4e, 0xd4, 0x65, 0x1b, 
-    0x84, 0xf7, 0x03, 0x60, 0x81, 0x5c, 
-    0x40, 0x9b, 0xcd, 0x25, 0x2a, 0xf8,    
+
+    // Replace with required MAC addresses
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, // Receiver address
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, // Transmitter address
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, // Destination address
+    
     0x00, 0x00, // sequence control
     0xaa, 0xaa, // SNAP
     0x03, 0x00, 0x00, 0x00, // other LLC headers
+
     0x08, 0x00,
     // IPv4 header
     0x45, //version :4 (obv) with IHL = 5
@@ -132,9 +71,9 @@ uint8_t packet[] = {
     0x00, 0x00, // Flags and fragment offset
     0x05, // TTL
     0x11, // Protocol type UDP 
-    0x33, 0x55, // Header checksum (update after calculating length of total packet)
+    0x33, 0x4a, // Header checksum
     0xc0, 0xa8, 0x00, 0x7A, // Source IP address - Arbitrary IP address for the ESP-32
-    0xc0, 0xa8, 0x00, 0xb7, // Destination IP address - Laptop's IP address (assigned through DHCP by the router)
+    0xc0, 0xa8, 0x00, 0xb8, // Destination IP address - Laptop's IP address (assigned through DHCP by the router)
 
     // UDP header - 8 bytes
     0x1f, 0x45, // Source port - Both source and destination ports are random since it is UDP
@@ -148,6 +87,7 @@ uint8_t packet[] = {
 
 };
 
+// Declare the DMA list item
 dma_list_item_t tx_item = {
     .size = 83,
     .length = 95,
@@ -157,7 +97,6 @@ dma_list_item_t tx_item = {
     .packet = &packet[0],
     .next = NULL
 };
-#endif
 
 // Event handlers for connecting to the wifi. If event notifies that station is started, perform connection
 // esp_wifi_connect() is part of the blobs
@@ -177,6 +116,47 @@ void event_handler(void *arg, esp_event_base_t event_base,
     }
 }
 
+void respect_mac_init(){
+    // ic_mac_init decompilation
+    uint32_t mac_val = REG_READ(WIFI_MAC_CTRL);
+    REG_WRITE(WIFI_MAC_CTRL, mac_val &  0xff00efff);
+
+}
+
+// Still needs the proprietary task to be running
+void respect_raw_tx(dma_list_item_t* tx_item){
+    // Write 0xa to the last byte (why?)
+    uint32_t cfg_val = REG_READ(WIFI_TX_CONFIG);
+    REG_WRITE(WIFI_TX_CONFIG, cfg_val | 0xa);
+
+    // Write the address of DMA struct to PLCP0 and set the 6th byte to 6 (why?) and 7th byte to 1 (why?)
+    REG_WRITE(WIFI_TX_PLCP0, (((uint32_t)tx_item & 0xfffff) | 0x600000) | 0x01000000);
+
+    // Using esp32-open-mac's implementation for reference
+    uint32_t rate = WIFI_PHY_RATE_54M;
+    uint32_t is_n_enabled = (rate >= 16);
+
+    REG_WRITE(WIFI_TX_PLCP1, 0x10000000 | (tx_item->length & 0xfff) | ((rate & 0x1f) << 12) | ((is_n_enabled & 0b1) << 25));
+    REG_WRITE(WIFI_TX_PLCP1_2, 0x0010000);
+
+    // Write 0x00000020 (why?)
+    REG_WRITE(WIFI_TX_PLCP2, 0x00000020);
+    
+    // Duration to 0 (Why?)
+    REG_WRITE(WIFI_TX_DURATION, 0x00);
+
+    // // Configure EDCA
+    cfg_val = REG_READ(WIFI_TX_CONFIG);
+    cfg_val = cfg_val | 0x02000000;
+    
+    cfg_val = cfg_val | 0x00003000;
+    REG_WRITE(WIFI_TX_CONFIG, cfg_val);
+
+    // Finally enable tx
+    cfg_val = REG_READ(WIFI_TX_PLCP0);
+    REG_WRITE(WIFI_TX_PLCP0, cfg_val | 0xc0000000);
+}
+
 
 void app_main(){
 
@@ -189,7 +169,7 @@ void app_main(){
     // initialize WiFi. This may have some hardware settings. Part of the .a files
     ret = esp_wifi_init(&cfg);
     if(ret != ESP_OK){
-        printf("Wifi could not be initialized (0x%x)\n", ret);
+        ESP_LOGE("Main", "Wifi could not be initialized %s", esp_err_to_name(ret));
     }
 
     // Create the event loop to monitor the wifi events such as connecting and station
@@ -205,8 +185,8 @@ void app_main(){
     // // Create WiFi configuration with defined SSID and PASSWORD
     wifi_config_t wifi_config = {
         .sta = {
-            .ssid = WIFI_SSID,
-            .password = WIFI_PASS,
+            .ssid = CONFIG_ESP_WIFI_SSID,
+            .password = CONFIG_ESP_WIFI_PASS,
         },
     };
 
@@ -218,70 +198,19 @@ void app_main(){
 
     vTaskDelay(2000 / portTICK_PERIOD_MS);
 
-    #ifdef SEND_RAW
     ESP_LOGW("Main", "Sending Raw packets");
+
+    // Killing proprietary wifi task seems to prevent any tx
     // ESP_LOGW("main", "Killing proprietary wifi task (ppTask)");
 	// pp_post(0xf, 0);
 
-
-    // // ic_mac_init decompilation
-    uint32_t mac_val = REG_READ(WIFI_MAC_CTRL);
-    REG_WRITE(WIFI_MAC_CTRL, mac_val &  0xff00efff);
-
-    #endif 
+    respect_mac_init();
 
     while(1){
         ESP_LOGI("Main", "Trying to send");
 
-#ifdef SEND_RAW
-        // Write 0xa to the last byte (why? - to enable tx?)
-        uint32_t cfg_val = REG_READ(WIFI_TX_CONFIG);
-        ESP_LOGI("WIFI_REG", "Value in %08x: %08x", (unsigned int)WIFI_TX_CONFIG, (unsigned int)REG_READ(WIFI_TX_CONFIG));
-        REG_WRITE(WIFI_TX_CONFIG, cfg_val | 0xa);
-        ESP_LOGI("WIFI_REG", "Value in %08x: %08x", (unsigned int)WIFI_TX_CONFIG, (unsigned int)REG_READ(WIFI_TX_CONFIG));
-
-        // Write the address of DMA struct to PLCP0 and set the 6th byte to 6 (why?) and 7th byte to 1 (why?)
-        REG_WRITE(WIFI_TX_PLCP0, (((uint32_t)&tx_item & 0xfffff) | 0x600000) | 0x01000000);
-        ESP_LOGI("WIFI_REG", "Value in %08x: %08x", (unsigned int)WIFI_TX_PLCP0, (unsigned int)REG_READ(WIFI_TX_PLCP0));
-
-        // Copied from Qemu output. Something to do with length of the packet
-        uint32_t rate = WIFI_PHY_RATE_54M;
-        uint32_t is_n_enabled = (rate >= 16);
-        REG_WRITE(WIFI_TX_PLCP1, 0x10000000 | (tx_item.length & 0xfff) | ((rate & 0x1f) << 12) | ((is_n_enabled & 0b1) << 25));
-        ESP_LOGI("WIFI_REG", "Value in %08x: %08x", (unsigned int)WIFI_TX_PLCP1, (unsigned int)REG_READ(WIFI_TX_PLCP1));
-
-        REG_WRITE(WIFI_TX_PLCP1_2, 0x0010000);
-        ESP_LOGI("WIFI_REG", "Value in %08x: %08x", (unsigned int)WIFI_TX_PLCP1_2, (unsigned int)REG_READ(WIFI_TX_PLCP1_2));
-
-        // Write 0x00000020 (why?)
-        REG_WRITE(WIFI_TX_PLCP2, 0x00000020);
-        ESP_LOGI("WIFI_REG", "Value in %08x: %08x", (unsigned int)WIFI_TX_PLCP2, (unsigned int)REG_READ(WIFI_TX_PLCP2));
-        
-        // Duration to 0 (Why?)
-        REG_WRITE(WIFI_TX_DURATION, 0x00);
-        ESP_LOGI("WIFI_REG", "Value in %08x: %08x", (unsigned int)WIFI_TX_DURATION, (unsigned int)REG_READ(WIFI_TX_DURATION));
-
-        // // Configure EDCA
-        cfg_val = REG_READ(WIFI_TX_CONFIG);
-        ESP_LOGI("WIFI_REG", "Value in %08x: %08x", (unsigned int)WIFI_TX_CONFIG, (unsigned int)REG_READ(WIFI_TX_CONFIG));
-        cfg_val = cfg_val | 0x02000000;
-        
-        cfg_val = cfg_val | 0x00003000;
-        REG_WRITE(WIFI_TX_CONFIG, cfg_val);
-        ESP_LOGI("WIFI_REG", "Value in %08x: %08x", (unsigned int)WIFI_TX_CONFIG, (unsigned int)REG_READ(WIFI_TX_CONFIG));
-
-        // Finally enable tx
-        printf("Enabling TX...\n");
-        cfg_val = REG_READ(WIFI_TX_PLCP0);
-        REG_WRITE(WIFI_TX_PLCP0, cfg_val | 0xc0000000);
-        ESP_LOGI("WIFI_REG", "Value in %08x: %08x", (unsigned int)WIFI_TX_PLCP0, (unsigned int)REG_READ(WIFI_TX_PLCP0));
-#else
-        ret = esp_wifi_internal_tx(WIFI_IF_STA, &udp_packet[0], sizeof(udp_packet));
-        ESP_LOGI("Main:", "Sent with result %s", esp_err_to_name(ret));
-#endif        
-
+        respect_raw_tx(&tx_item);
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        
     }
 
 }
